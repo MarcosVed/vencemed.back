fa4e85f2b25361433e4964b64aa70b049ca2ddea
edkkkj <marcosfransa910@gmail.com>
2025-09-18 08:20:34 -0300

    commit 18/09/25

---
+++ Vencemed/src/main/java/itb/grupo6/vencemed/Vencemed/VencemedApplication.java
@@ -0,0 +1,13 @@
+package itb.grupo6.vencemed.Vencemed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VencemedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VencemedApplication.class, args);
+	}
+
+}
---
+++ Vencemed/src/test/java/itb/grupo6/vencemed/Vencemed/VencemedApplicationTests.java
@@ -0,0 +1,13 @@
+package itb.grupo6.vencemed.Vencemed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VencemedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
